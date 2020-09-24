eb258d36e8f6370edc6d0237f88fae873919cbec
CrispyPin <CrispyPin@users.noreply.github.com>
2020-09-24 14:15:59 +0200

    add ability to set port on server, needs testing

--- server/Server.gd
+++ server/Server.gd
@@ -1,19 +1,27 @@
-extends Label
+extends Node2D
+
+func start_server(port, max_users):
+	var network = NetworkedMultiplayerENet.new()
+	network.create_server(port, max_users)# port, max users
+	get_tree().set_network_peer(network)
+
+	$StatusLabel.text += "Server started on port " + str(port) + "\n"
+
+	network.connect("peer_connected",self,"_peer_connected")
+	network.connect("peer_disconnected",self,"_peer_disconnected")
+	
 
 func _ready():
-  var network = NetworkedMultiplayerENet.new()
-  network.create_server(4242, 2)# port, max users
-  get_tree().set_network_peer(network)
-
-  text = text + "Server started"
-
-  network.connect("peer_connected",self,"_peer_connected")
-  network.connect("peer_disconnected",self,"_peer_disconnected")
+  start_server(4242, 2)
   
 func _peer_connected(id):
-  text = text + "\nUser " + str(id) + " connected"
-  get_parent().get_node("UserCount").text = "Total Users:" + str(get_tree().get_network_connected_peers().size())
+	$StatusLabel.text += "\nUser " + str(id) + " connected"
+	$UserCount.text = "Total Users:" + str(get_tree().get_network_connected_peers().size())
   
 func _peer_disconnected(id):
-  text = text + "\nUser " + str(id) + " disconnected"
-  get_parent().get_node("UserCount").text = "Total Users:" + str(get_tree().get_network_connected_peers().size())
+	$statusLabel.text += "\nUser " + str(id) + " disconnected"
+	$UserCount.text = "Total Users:" + str(get_tree().get_network_connected_peers().size())
+
+
+func _on_ConfirmPort_pressed():
+	start_server($PortField.text.to_int(), 2)
--- voxel-game/Scripts/ConnectionMenu.gd
+++ voxel-game/Scripts/ConnectionMenu.gd
@@ -5,6 +5,8 @@ func _process(_delta):
 		self.visible = !self.visible
 		if self.visible:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_node("../Player").pause_mode = Node.PAUSE_MODE_STOP
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			get_node("../Player").pause_mode = Node.PAUSE_MODE_INHERIT
 
