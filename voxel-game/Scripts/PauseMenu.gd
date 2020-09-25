ce774fa9088ea9ae2320817125d12ffcbce30c80
CrispyPin <CrispyPin@users.noreply.github.com>
2020-09-25 08:59:42 +0200

    pause menu actually pauses game

--- voxel-game/Scripts/PauseMenu.gd
+++ voxel-game/Scripts/PauseMenu.gd
@@ -1,12 +1,15 @@
 extends Panel
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _process(_delta):
 	if Input.is_action_just_pressed("pause_menu"):
 		self.visible = !self.visible
 		if self.visible:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			get_node("../Player").pause_mode = Node.PAUSE_MODE_STOP
+			get_tree().paused = true
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			get_node("../Player").pause_mode = Node.PAUSE_MODE_INHERIT
+			get_tree().paused = false
 
