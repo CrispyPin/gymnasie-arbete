95a8e84ebfc718b0d327c781411076df2c6da329
CrispyPin <CrispyPin@users.noreply.github.com>
2020-10-09 09:38:59 +0200

    fix respawn height

--- voxel-game/Scripts/Player.gd
+++ voxel-game/Scripts/Player.gd
@@ -75,7 +75,7 @@ func _physics_process(delta):
 		rpc_unreliable("update_pos", global_transform)
 
 func respawn():
-	global_transform.origin = Vector3(0,1,0)
+	global_transform.origin = Vector3(0, 5, 0)
 	velocity = Vector3(0, 0, 0);
 	rpc_unreliable("update_pos", global_transform)
 
