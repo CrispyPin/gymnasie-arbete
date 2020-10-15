0442b8e29aafa3e295a8da19db3490dcc1711ba1
CrispyPin <CrispyPin@users.noreply.github.com>
2020-10-15 10:35:42 +0200

    rework main menu

--- voxel-game/Scripts/MainMenu.gd
+++ voxel-game/Scripts/MainMenu.gd
@@ -3,21 +3,34 @@ extends Panel
 var lobby
 
 export var ip = "127.0.0.1"
-export var port = 4242
+export var join_port = 4242
+
+export var host_port = 4242
+export var max_players = 16
 
 func _ready():
 	lobby = get_parent()
 
 
-func _on_HostButton_pressed():
-	lobby.host_game(port, 16)
+func _host_game():
+	lobby.host_game(host_port, max_players)
 
-func _on_JoinButton_pressed():
-	lobby.join_game(ip, port)
+func _join_game():
+	lobby.join_game(ip, join_port)
 
 
-func _on_InputIP_text_changed(new_text):
+func _set_ip(new_text):
 	ip = new_text
 
-func _on_InputPort_text_changed(new_text):
-	port = int(new_text)
+func _set_join_port(new_text):
+	join_port = int(new_text)
+
+func _set_host_port(new_text):
+	host_port = int(new_text)
+
+func _set_max_players(new_text):
+	max_players = int(new_text)
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
